418e513563db315e716bb3d84c15e8af659a6180
Banuep <50018359+Banuep@users.noreply.github.com>
2026-05-15 15:19:54 +0700

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,20 +1,4 @@
 export default async function handler(req, res) {
   const { city } = req.query;
-
-  // Validasi input
-  if (!city) {
-    return res.status(400).json({ error: { message: 'Parameter kota tidak boleh kosong.' } });
-  }
-
-  try {
-    const response = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${encodeURIComponent(city)}&lang=id`
-    );
-
-    const data = await response.json();
-    res.status(200).json(data);
-
-  } catch (err) {
-    res.status(500).json({ error: { message: 'Gagal mengambil data cuaca.' } });
-  }
+  ...
 }
